1b1aad5e8e23ac18a0f69d7c80da2ef686662ff6
Tahsina Islam <ti429@nyu.edu>
2022-06-01 02:54:35 -0400

    commit

--- SemanticNetsAgent.py
+++ SemanticNetsAgent.py
@@ -1,6 +1,3 @@
-import copy
-import queue 
-
 from collections import deque
 
 class SemanticNetsAgent:
@@ -40,6 +37,15 @@ class SemanticNetsAgent:
                     else:
                         return False  
 
+        def checkVisit(currchild,closed_array):
+            check = True
+            for object in closed_array:
+                if (currchild.left_sheep == object.left_sheep and currchild.left_wolf == object.left_wolf and currchild.boat == object.boat):
+                    check = False
+            return check
+
+
+
         def findKids(currNode):
             children = []
             movesarray = [(2,0),(0,2),(1,1),(0,1),(1,0)]
@@ -58,6 +64,7 @@ class SemanticNetsAgent:
             return children
 
 
+
         goal_state = False
         root = State(initial_sheep, initial_wolves, 0, 0, "left")
         open_states = deque([])
@@ -66,19 +73,18 @@ class SemanticNetsAgent:
         while (goal_state == False and open_states):
             currState = open_states.popleft()
             if (currState.isGoalState()):
-                goal_state = True
+                    goal_state = True
             else:
                 closed_states.append(currState)
                 newchildren = findKids(currState)
                 for child in newchildren:
-                    if child not in closed_states:
+                    if (checkVisit(child,closed_states) and checkVisit(child,open_states)):
                         open_states.append(child)
-
         if goal_state == True:
             all_moves = []
             order_moves = []
             while currState.parent:
-                move = (abs(currState.left_sheep-currState.parent.leftsheep), abs(currState.left_wolf-currState.parent.left_wolf))
+                move = (abs(currState.left_sheep-currState.parent.left_sheep), abs(currState.left_wolf-currState.parent.left_wolf))
                 all_moves.append(move)
                 currState=currState.parent
             for i in range(len(all_moves)):
